c7a68998574b28526233d57343a193f741487a03
Mara Damatac <mos.damatac@gmail.com>
2016-06-01 00:31:34 -0700

    Added data class 'Member'

---
+++ A00973641_assignment1/src/a00973641/data/Member.java
@@ -0,0 +1,161 @@
+/**
+ * Project: A00973641_assignment1
+ * File: Member.java
+ * Date: Jun 1, 2016
+ * Time: 12:27:10 AM
+ */
+package a00973641.data;
+
+import java.io.Serializable;
+
+/**
+ * @author Mara
+ *
+ */
+public class Member implements Serializable {
+
+	private int memberID;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String code;
+	private String country;
+	private String phoneNumber;
+	private String email;
+
+	/**
+	 * @return the memberID
+	 */
+	public int getMemberID() {
+		return memberID;
+	}
+
+	/**
+	 * @param memberID
+	 *            the memberID to set
+	 */
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code
+	 *            the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country
+	 *            the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *            the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
