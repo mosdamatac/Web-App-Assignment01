d58c420aa55d93ca467176e8d3071eda49058969
Mara Damatac <mos.damatac@gmail.com>
2016-06-01 15:38:56 -0700

    Added member retrieval in servlet from database

--- A00973641_assignment1/src/a00973641/Assignment01Servlet.java
+++ A00973641_assignment1/src/a00973641/Assignment01Servlet.java
@@ -1,14 +1,21 @@
 package a00973641;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import a00973641.data.Member;
 import a00973641.database.Database;
+import a00973641.database.DbConstants;
 
 /**
  * Servlet implementation class Assignment01Servlet
@@ -24,8 +31,31 @@ public class Assignment01Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Database db = Database.getInstance();
+		Connection dbConn = null;
+		PreparedStatement ps = null;
+		String selectSQL = "SELECT * FROM " + DbConstants.MEMBER_TABLE_NAME;
 		try {
 			db.init();
+			dbConn = db.getConnection();
+			ps = dbConn.prepareStatement(selectSQL);
+
+			Member member;
+			List<Member> memberList = new ArrayList<>();
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				member = new Member();
+				member.setMemberID(rs.getInt(1));
+				member.setFirstName(rs.getString(2));
+				member.setLastName(rs.getString(3));
+				member.setAddress(rs.getString(4));
+				member.setCity(rs.getString(5));
+				member.setCode(rs.getString(6));
+				member.setCountry(rs.getString(7));
+				member.setPhoneNumber(rs.getString(8));
+				member.setEmail(rs.getString(9));
+
+				memberList.add(member);
+			}
 		} catch (SQLException e) {
 			// TODO error
 		}
